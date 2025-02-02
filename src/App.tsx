dbc76d24e153949f1fcacff575ddb1e048702049
sarathcodes516 <sarath51k6@gmail.com>
2025-02-02 11:26:14 +0530

    abc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LandingPage } from './pages/LandingPage';
 import { PropertiesPage } from './pages/PropertiesPage';
 import { PropertyPage } from './pages/PropertyPage';
 
